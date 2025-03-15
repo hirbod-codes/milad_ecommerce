e31e3cb1bc32b389fda143dacdb02971292be8ce
Hirbod Khatami <hirbod.khatami.words@gmail.com>
2025-03-15 15:05:46 +0000

    Update Category collection

--- backend/API/src/DB/Models/Category.ts
+++ backend/API/src/DB/Models/Category.ts
@@ -3,6 +3,8 @@ import { InferType, mixed, number, object, string } from "yup";
 
 export const collectionName = 'category'
 
+export const schemaVersion = 'v1.0.0'
+
 export const categorySchema = object().required().noUnknown(true).strict(true).shape({
     schemaVersion: string().required().min(6).max(20),
     _id: mixed<string | ObjectId>().required(),
@@ -14,9 +16,18 @@ export const categorySchema = object().required().noUnknown(true).strict(true).s
 })
 export type Category = InferType<typeof categorySchema>
 
+export const categoryInputSchema = categorySchema.required().noUnknown(true).strict(true).pick(['parentCategory', 'name'])
+export type CategoryInput = InferType<typeof categoryInputSchema>
+
+export const categoryCreateSchema = categorySchema.required().noUnknown(true).strict(true).omit(['_id']).shape({ _id: mixed<string | ObjectId>().optional() })
+export type CategoryCreate = InferType<typeof categoryCreateSchema>
+
+export const categoryUpdateSchema = categorySchema.required().noUnknown(true).strict(true).pick(['views'])
+export type CategoryUpdate = InferType<typeof categoryUpdateSchema>
+
+export const categoryImmutableSchema = categorySchema.required().noUnknown(true).strict(true).pick(Object.keys(categorySchema.fields).filter(f => !['_id', 'schemaVersion', 'createdAt', 'updatedAt'].concat(Object.keys(categoryUpdateSchema.fields)).includes(f)) as any)
+export type CategoryImmutable = InferType<typeof categoryImmutableSchema>
+
 export const fields: string[] = Object.keys(categorySchema.fields)
 export const readableFields = fields.filter(f => !['schemaVersion'].includes(f))
 export const updatableFields = []
-
-export const categoryCreateSchema = categorySchema.required().noUnknown(true).strict(true).omit(['_id', 'schemaVersion', 'createdAt', 'updatedAt', 'views'])
-export type CategoryCreate = InferType<typeof categoryCreateSchema>
---
+++ backend/API/src/DB/Repositories/CategoryRepository.ts
@@ -0,0 +1,44 @@
+import { Collection, DeleteResult, InsertOneResult, ObjectId, UpdateResult } from 'mongodb/mongodb'
+import { DateTime } from 'luxon'
+import { Category, CategoryCreate, CategoryInput, CategoryUpdate, schemaVersion } from '../Models/Category'
+
+export class CategoryRepository {
+    private collection: Collection<CategoryCreate>
+
+    constructor(collection: Collection<CategoryCreate>) {
+        this.collection = collection
+    }
+
+    async create(category: CategoryInput): Promise<InsertOneResult | false> {
+        const ts = DateTime.utc().toUnixInteger()
+
+        let o: CategoryCreate = {
+            ...category,
+            schemaVersion,
+            views: 0,
+            createdAt: ts,
+            updatedAt: ts,
+        }
+
+        try { return await this.collection.insertOne(o) }
+        catch (e) {
+            console.error(e)
+            return false
+        }
+    }
+
+    async getById(id: string): Promise<Category | null | undefined> {
+        try { return await this.collection.findOne({ _id: ObjectId.createFromHexString(id) }) }
+        catch (e) { console.error(e); return undefined }
+    }
+
+    async updateById(id: string, category: CategoryUpdate): Promise<UpdateResult | false> {
+        try { return await this.collection.updateOne({ _id: ObjectId.createFromHexString(id) }, { ...category, updatedAt: DateTime.utc().toUnixInteger() }) }
+        catch (e) { console.error(e); return false }
+    }
+
+    async deleteById(id: string): Promise<DeleteResult | false> {
+        try { return await this.collection.deleteOne({ _id: ObjectId.createFromHexString(id) }) }
+        catch (e) { console.error(e); return false }
+    }
+}
