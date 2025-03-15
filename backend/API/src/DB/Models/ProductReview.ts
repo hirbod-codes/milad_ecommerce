6baf98d97cb8351005f7c5278fa1682b80066f67
Hirbod Khatami <hirbod.khatami.words@gmail.com>
2025-03-15 16:05:38 +0000

    Update ProductReview collection

--- backend/API/src/DB/Models/ProductReview.ts
+++ backend/API/src/DB/Models/ProductReview.ts
@@ -3,6 +3,8 @@ import { InferType, mixed, number, object, string } from "yup";
 
 export const collectionName = 'productReview'
 
+export const schemaVersion = 'v1.0.0'
+
 export const productReviewSchema = object().required().noUnknown(true).strict(true).shape({
     schemaVersion: string().required().min(6).max(20),
     _id: mixed<string | ObjectId>().required(),
@@ -15,9 +17,18 @@ export const productReviewSchema = object().required().noUnknown(true).strict(tr
 })
 export type ProductReview = InferType<typeof productReviewSchema>
 
-export const fields: string[] = Object.keys(productReviewSchema.fields)
-export const readableFields = fields.filter(f => !['schemaVersion'].includes(f))
-export const updatableFields = []
+export const productReviewInputSchema = productReviewSchema.required().noUnknown(true).strict(true).pick(['productId', 'userId', 'content', 'rating'])
+export type ProductReviewInput = InferType<typeof productReviewInputSchema>
 
-export const productReviewCreateSchema = productReviewSchema.required().noUnknown(true).strict(true).pick(['content', 'rating', 'productId'])
+export const productReviewCreateSchema = productReviewSchema.required().noUnknown(true).strict(true).omit(['_id']).shape({ _id: mixed<string | ObjectId>().optional() })
 export type ProductReviewCreate = InferType<typeof productReviewCreateSchema>
+
+export const productReviewUpdateSchema = productReviewSchema.required().noUnknown(true).strict(true).pick(['content', 'rating'])
+export type ProductReviewUpdate = InferType<typeof productReviewUpdateSchema>
+
+export const productReviewImmutableSchema = productReviewSchema.required().noUnknown(true).strict(true).pick(Object.keys(productReviewSchema.fields).filter(f => !['_id', 'schemaVersion', 'createdAt', 'updatedAt'].concat(Object.keys(productReviewUpdateSchema.fields)).includes(f)) as any)
+export type ProductReviewImmutable = InferType<typeof productReviewImmutableSchema>
+
+export const fields: (keyof ProductReview)[] = Object.keys(productReviewSchema.fields) as any
+export const readableFields: (keyof Omit<ProductReview, 'schemaVersion'>)[] = fields.filter(f => !['schemaVersion'].includes(f)) as any
+export const updatableFields: (keyof ProductReviewUpdate)[] = Object.keys(productReviewUpdateSchema.fields) as any
---
+++ backend/API/src/DB/Repositories/ProductReviewsRepository.ts
@@ -0,0 +1,40 @@
+import { Collection, DeleteResult, InsertOneResult, ObjectId, UpdateResult } from 'mongodb/mongodb'
+import { ProductReview, ProductReviewCreate, ProductReviewInput, ProductReviewUpdate, schemaVersion } from '../Models/ProductReview'
+import { DateTime } from 'luxon'
+
+export class ProductReviewsRepository {
+    private collection: Collection<ProductReviewCreate>
+
+    constructor(collection: Collection<ProductReviewCreate>) {
+        this.collection = collection
+    }
+
+    async create(order: ProductReviewInput): Promise<InsertOneResult | false> {
+        const ts = DateTime.utc().toUnixInteger()
+
+        let pr: ProductReviewCreate = {
+            ...order,
+            schemaVersion,
+            createdAt: ts,
+            updatedAt: ts,
+        }
+
+        try { return await this.collection.insertOne(pr) }
+        catch (e) { console.error(e); return false }
+    }
+
+    async getById(id: string): Promise<ProductReview | null | undefined> {
+        try { return await this.collection.findOne({ _id: ObjectId.createFromHexString(id) }) }
+        catch (e) { console.error(e); return undefined }
+    }
+
+    async updateById(id: string, order: ProductReviewUpdate): Promise<UpdateResult | false> {
+        try { return await this.collection.updateOne({ _id: ObjectId.createFromHexString(id) }, { ...order, updatedAt: DateTime.utc().toUnixInteger() }) }
+        catch (e) { console.error(e); return false }
+    }
+
+    async deleteById(id: string): Promise<DeleteResult | false> {
+        try { return await this.collection.deleteOne({ _id: ObjectId.createFromHexString(id) }) }
+        catch (e) { console.error(e); return false }
+    }
+}
