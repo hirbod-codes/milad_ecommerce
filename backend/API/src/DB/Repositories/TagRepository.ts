234ca3f216ae274fab4953fe08626696a6db5bfd
Hirbod Khatami <hirbod.khatami.words@gmail.com>
2025-03-15 21:59:38 +0000

    Update TagRepository.ts

--- backend/API/src/DB/Repositories/TagRepository.ts
+++ backend/API/src/DB/Repositories/TagRepository.ts
@@ -21,10 +21,7 @@ export class TagRepository {
         }
 
         try { return await this.collection.insertOne(o) }
-        catch (e) {
-            console.error(e)
-            return false
-        }
+        catch (e) { console.error(e); return false }
     }
 
     async getById(id: string): Promise<Tag | null | undefined> {
