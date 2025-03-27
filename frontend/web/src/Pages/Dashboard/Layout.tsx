cdb2f461ba9b42e8ab3c961872f8c9a443ae5928
Hirbod Khatami <hirbod.khatami.words@gmail.com>
2025-03-27 08:12:06 +0000

    Update Layout.tsx

--- frontend/web/src/Pages/Dashboard/Layout.tsx
+++ frontend/web/src/Pages/Dashboard/Layout.tsx
@@ -1,16 +1,30 @@
 import { Stack } from "@/src/Components/Base/Stack"
 import { Navigation } from "@/src/Components/Navigation"
-import { useEffect, useRef } from "react"
+import { useEffect, useReducer, useRef } from "react"
 import { Outlet } from "react-router"
 
 export function Layout() {
+    const ref = useRef<HTMLDivElement>(null)
+    const grownRef = useRef<HTMLDivElement>(null)
+
+    const [, rerender] = useReducer(x => x + 1, 0)
+
+    console.log('Dashboard Layout')
+
+    useEffect(() => {
+        if (ref.current) {
+            grownRef.current.style.width = (ref.current.getBoundingClientRect().width - 64) + 'px'
+            rerender()
+        }
+    }, [ref, ref.current])
+
     return (
-        <Stack stackProps={{ className: 'size-full', }}>
+        <Stack stackProps={{ className: 'size-full', }} stackRef={ref}>
             <div className="w-[64px] h-full">
                 <Navigation />
             </div>
 
-            <div className="flex-grow h-full">
+            <div className="h-full" ref={grownRef}>
                 <Outlet />
             </div>
         </Stack >
